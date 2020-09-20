4ba2da8e380d16fb97591f23cf30f3c08f32e17e
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 09:31:07 -0400

    failing next_quality is a function

---
+++ src/__tests__/gilded_rose.next_quality.test.js
@@ -0,0 +1,5 @@
+const { next_quality } = require('../gilded_rose');
+
+describe('gilded_rose next_quality()', () => {
+  it('is a function', () => expect(typeof next_quality).toEqual('function'));
+});
