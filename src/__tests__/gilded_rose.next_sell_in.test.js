103681b9c6fe9141c887f56c78d30c1a98cab052
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 09:11:01 -0400

    failing decrements the sell_in

--- src/__tests__/gilded_rose.next_sell_in.test.js
+++ src/__tests__/gilded_rose.next_sell_in.test.js
@@ -2,4 +2,17 @@ const { next_sell_in } = require('../gilded_rose');
 
 describe('gilded_rose next_sell_in()', () => {
   it('is a function', () => expect(typeof next_sell_in).toEqual('function'));
+
+  it('that decrements the sell_in of categorized item', () => {
+    const given = {
+      categoryName: 'Standard',
+      item: { sell_in: 1 },
+    }
+    const expected = {
+      categoryName: 'Standard',
+      item: { sell_in: 0 },
+    }
+    const received = next_sell_in(given);
+    expect(received).toEqual(expected);
+  });
 });
