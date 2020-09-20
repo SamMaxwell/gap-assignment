b8c3041ba6663faeb1175b3617ce32c98b2d2df9
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 09:17:54 -0400

    failing next_sell_in Sulfuras

--- src/__tests__/gilded_rose.next_sell_in.test.js
+++ src/__tests__/gilded_rose.next_sell_in.test.js
@@ -13,4 +13,15 @@ describe('gilded_rose next_sell_in()', () => {
     const received = given.item.sell_in;
     expect(received).toEqual(expected);
   });
+
+  it('unless the item is Sulfuras', () => {
+    const given = {
+      categoryName: 'Sulfuras',
+      item: { sell_in: 1 },
+    }
+    const expected = 1;
+    next_sell_in(given);
+    const received = given.item.sell_in;
+    expect(received).toEqual(expected);
+  })
 });
