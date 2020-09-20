3d82a0c0a4c3ca357e24028cef87661759fe9899
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 09:50:04 -0400

    failing next_quality that degrades the quality

--- src/__tests__/gilded_rose.next_quality.test.js
+++ src/__tests__/gilded_rose.next_quality.test.js
@@ -2,4 +2,14 @@ const { next_quality } = require('../gilded_rose');
 
 describe('gilded_rose next_quality()', () => {
   it('is a function', () => expect(typeof next_quality).toEqual('function'));
+
+  it('that degrades the quality of an item', () => {
+    const given = {
+      item: { quality: 10 },
+    }
+    const expected = 9;
+    next_quality(given);
+    const received = given.item.quality;
+    expect(received).toEqual(expected);
+  });
 });
--- src/__tests__/gilded_rose.next_sell_in.test.js
+++ src/__tests__/gilded_rose.next_sell_in.test.js
@@ -3,9 +3,8 @@ const { next_sell_in } = require('../gilded_rose');
 describe('gilded_rose next_sell_in()', () => {
   it('is a function', () => expect(typeof next_sell_in).toEqual('function'));
 
-  it('that decrements the sell_in of a categorized item', () => {
+  it('that decrements the sell_in of an item', () => {
     const given = {
-      categoryName: 'Standard',
       item: { sell_in: 1 },
     }
     const expected = 0;
