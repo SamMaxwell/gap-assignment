4a0248f39a0c3678bafca84195b6ccf93a06e6af
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 10:59:17 -0400

    failing increases Backstage Pass quality by three

--- src/__tests__/gilded_rose.next_quality.test.js
+++ src/__tests__/gilded_rose.next_quality.test.js
@@ -87,4 +87,15 @@ describe('gilded_rose next_quality()', () => {
     const received = given.item.quality;
     expect(received).toEqual(expected);
   });
+
+  it('increases Backstage Pass quality by three when five days or less', () => {
+    const given = {
+      categoryName: 'Backstage Pass',
+      item: { sell_in: 5, quality: 1 },
+    }
+    const expected = 4;
+    next_quality(given);
+    const received = given.item.quality;
+    expect(received).toEqual(expected);
+  });
 });
