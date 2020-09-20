50015d5c6411e54a6e3c23f7174940854a1ca1f2
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 09:03:11 -0400

    failing next_sell_in is a function

---
+++ src/__tests__/gilded_rose.next_sell_in.test.js
@@ -0,0 +1,5 @@
+const { next_sell_in } = require('../gilded_rose');
+
+describe('gilded_rose next_sell_in()', () => {
+  it('is a function', () => expect(typeof next_sell_in).toEqual('function'));
+});
