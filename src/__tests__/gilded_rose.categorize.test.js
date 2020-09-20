dc1fa53566b4a622a8915dbade41e8e189e265af
Sam Maxwell <sam.maxwell@teamdynamix.com>
2020-09-20 08:59:44 -0400

    passing that creates an array of categorized items

--- src/__tests__/gilded_rose.categorize.test.js
+++ src/__tests__/gilded_rose.categorize.test.js
@@ -5,7 +5,14 @@ describe('gilded_rose categorize()', () => {
 
   it('that creates an array of categorized items', () => {
     const received = categorize();
-    const expected = null;
+    const expected = [
+      {'categoryName': 'Standard', 'item': {'name': '+5 Dexterity Vest', 'quality': 20, 'sell_in': 10}},
+      {'categoryName': 'Aged Brie', 'item': {'name': 'Aged Brie', 'quality': 0, 'sell_in': 2}},
+      {'categoryName': 'Standard', 'item': {'name': 'Elixir of the Mongoose', 'quality': 7, 'sell_in': 5}},
+      {'categoryName': 'Sulfuras', 'item': {'name': 'Sulfuras, Hand of Ragnaros', 'quality': 80, 'sell_in': 0}},
+      {'categoryName': 'Backstage Pass', 'item': {'name': 'Backstage passes to a TAFKAL80ETC concert', 'quality': 20, 'sell_in': 15}},
+      {'categoryName': 'Standard', 'item': {'name': 'Conjured Mana Cake', 'quality': 6, 'sell_in': 3}}
+    ];
     expect(received).toEqual(expected);
   });
 });
